c48b1bf0ea0b631f74c163adcb528427311d61ea
Sinan <sinanjasar@live.dk>
2019-11-25 17:50:48 +0100

    added search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Search.css';
+function Search() {
+    return <div>
+        <h1>Search for a city</h1>
+        <div className="search-bar"><input type="text"></input><i>Search</i></div>
+    </div>
+}
+
+export default Search;
