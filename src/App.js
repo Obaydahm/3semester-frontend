592d166c46cedf59289d5ece3aa0b0bfa6678e9b
Sinan <sinanjasar@live.dk>
2019-11-25 21:09:45 +0100

    styled, added localstorage functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import { Route, BrowserRouter as Router } from "react-router-dom"
-import Search from "./components/Search/Search"
-import './App.css';
+import React, { useState } from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Search from "./components/Search/Search";
+import "./App.css";
 
 function App() {
+  const [searches, setSearches] = useState([]);
+  const [city, setCity] = useState({});
+  /*initialize city on fetch method and pass as props to Search component. Then it can be added to list of latest searches*/
   return (
     <div className="App">
       <Router>
-        <Route path="/search" component={Search} />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/search">
+          <Search searches={searches} city={city} setSearches={setSearches} />
+        </Route>
       </Router>
     </div>
   );
 }
-
+function Home() {
+  return <div>{localStorage.getItem("searches")}</div>;
+}
 export default App;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,58 @@
-import React from 'react';
-import './Search.css';
-function Search() {
-    return <div>
-        <h1>Search for a city</h1>
-        <div className="search-bar"><input type="text"></input><i>Search</i></div>
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import magnifier from "./icons/magnifying-glass.svg";
+import exit from "./icons/exit.svg";
+import "./Search.css";
+function Search({ searches, setSearches }) {
+  const [search, setSearch] = useState();
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    searches = localStorage.getItem("searches").split(",");
+    searches.unshift(
+      search.charAt(0).toUpperCase() + search.substring(1).toLowerCase()
+    );
+    if (searches.length > 5) {
+      searches.pop();
+    }
+    setSearches(searches);
+    setSearch("");
+    localStorage.setItem("searches", searches);
+  }
+  return (
+    <div className="search-wrapper">
+      <Link to="/">
+        <img src={exit} className="search-exit" />
+      </Link>
+      <h1>Search for a city</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="search-bar">
+          <input
+            onChange={handleChange}
+            type="text"
+            value={search}
+            placeholder="Search for a city ..."
+          ></input>
+          <submit>
+            <img className="search-magnifier" src={magnifier} />
+          </submit>
+        </div>
+      </form>
+      <h4>Latest searches</h4>
+      <ul>
+        {localStorage
+          .getItem("searches")
+          .split(",")
+          .map((search, index) => (
+            <li key={index}>
+              <Link to={search}>{search}</Link>
+            </li>
+          ))}
+      </ul>
     </div>
+  );
 }
 
 export default Search;
