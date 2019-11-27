004c6129827fa1f6bbd69ec01ae1f9e475ada4b9
Sinan <sinanjasar@live.dk>
2019-11-27 13:01:25 +0100

    added comments and pushed searched city to history

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,50 +1,56 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import magnifier from "./icons/magnifying-glass.svg";
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import history from "../../history";
 import exit from "./icons/exit.svg";
 import "./Search.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-function Search({ searches, setSearches }) {
-
+function Search({ searches, setSearches, search, setSearch }) {
   const iconCloudStyle = {
-    fontSize: '60px',
-    color: '#f2f8ff'
+    fontSize: "60px",
+    color: "#f2f8ff"
   };
-
   const iconSunStyle = {
-    fontSize: '30px',
-    color: 'yellow',
-    marginTop: '27px',
-    marginLeft: '-5px',
+    fontSize: "30px",
+    color: "yellow",
+    marginTop: "27px",
+    marginLeft: "-5px"
   };
 
   const spanStyle = {
-    padding: '45px 0px'
+    padding: "45px 0px"
   };
+  const history = useHistory();
 
-  /*localStorage.setItem("searches", "");*/
-  const [search, setSearch] = useState();
   function handleChange(event) {
     setSearch(event.target.value);
   }
+
   function handleSubmit(event) {
     event.preventDefault();
+
     if (localStorage.getItem("searches") !== null) {
       searches = localStorage.getItem("searches").split(",");
     }
+
+    /* If search already exists, remove and at to front */
     searches = searches.filter(s => s.toLowerCase() !== search.toLowerCase());
+    const capitalized =
+      search.charAt(0).toUpperCase() + search.substring(1).toLowerCase();
+    searches.unshift(capitalized);
+    /* ------------------------------------------------ */
 
-    searches.unshift(
-      search.charAt(0).toUpperCase() + search.substring(1).toLowerCase()
-    );
-
+    /* If there are more than 5 searches, remove the first search */
     if (searches.length > 5) {
       searches.pop();
     }
+    /* ---------------------------------------------------------- */
+
     setSearches(searches);
     setSearch("");
     localStorage.setItem("searches", searches);
+    history.push("/" + capitalized);
   }
+
   return (
     <div className="search-wrapper">
       <Link to="/">
@@ -66,26 +72,32 @@ function Search({ searches, setSearches }) {
             placeholder="Search for a city ..."
           ></input>
           <button onClick={handleSubmit}>
-            <FontAwesomeIcon icon={['fal', 'search-location']} size="2x" className="search-magnifier" />
+            <FontAwesomeIcon
+              icon={["fal", "search-location"]}
+              className="search-magnifier"
+            />
           </button>
         </div>
       </form>
       <h4>Latest searches</h4>
       <ul>
-        {
-          localStorage.getItem("searches") !== null ? (
-            localStorage
-              .getItem("searches")
-              .split(",")
-              .map((search, index) => (
-                <li key={index}>
-                  <Link to={search}>{search}</Link>
-                </li>
-              ))
-          ) : (
-              <p>Nothing to see here <span role="img" aria-label="wink-smiley">&#128521;</span></p>
-            )
-        }
+        {localStorage.getItem("searches") !== null ? (
+          localStorage
+            .getItem("searches")
+            .split(",")
+            .map((search, index) => (
+              <li key={index}>
+                <Link to={search}>{search}</Link>
+              </li>
+            ))
+        ) : (
+          <p>
+            Nothing to see here{" "}
+            <span role="img" aria-label="wink-smiley">
+              &#128521;
+            </span>
+          </p>
+        )}
       </ul>
     </div>
   );
