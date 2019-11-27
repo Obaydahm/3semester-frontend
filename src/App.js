463faa94149969a8d01ec199ef6e9c9e78be1395
Sinan <sinanjasar@live.dk>
2019-11-27 13:02:30 +0100

    updated props

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/pro-solid-svg-icons';
-import { fal } from '@fortawesome/pro-light-svg-icons';
-import facade from './apiFacade';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/pro-solid-svg-icons";
+import { fal } from "@fortawesome/pro-light-svg-icons";
 import Search from "./components/Search/Search";
 import CityInfo from "./components/CityInfo/CityInfo";
 import "./App.css";
-
-function App() {
+function App({ facade }) {
   library.add(fas, fal);
-
   const [searches, setSearches] = useState([]);
-  const [city, setCity] = useState({});
-  /*initialize city on fetch method and pass as props to Search component. Then it can be added to list of latest searches*/
+  const [search, setSearch] = useState("");
+  const fetchCity = city => facade.fetchCityInfo(city);
+  let city = {};
+
   return (
     <div className="App">
       <Router>
         <Route exact path="/">
-          <Home />
+          <Home fetchCity={fetchCity} search={search} />
         </Route>
         <Route path="/search">
-          <Search searches={searches} city={city} setSearches={setSearches} facade={facade} />
+          <Search
+            searches={searches}
+            setSearches={setSearches}
+            search={search}
+            setSearch={setSearch}
+          />
         </Route>
         <Route path="/cityinfo">
-          <CityInfo city={city} facade={facade} />
+          <CityInfo facade={facade} />
         </Route>
       </Router>
     </div>
   );
 }
-function Home() {
-
+function Home({ fetchCity, search }) {
+  let cityInfo = {};
   const linkStyle = {
-    fontSize: '20px',
-    color: '#999999'
+    fontSize: "20px",
+    color: "#999999"
   };
-
   return (
     <div>
       {localStorage.getItem("searches")}
       <br />
-      <Link to="/search" style={linkStyle}>Search</Link><br />
-      <Link to="/cityinfo" style={linkStyle}>City Info</Link>
+      <Link to="/search" style={linkStyle}>
+        Search
+      </Link>
+      <br />
+      <Link to="/cityinfo" style={linkStyle}>
+        City Info
+      </Link>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import apiFacade from "./apiFacade";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App facade={apiFacade} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
